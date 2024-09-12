358b2c197b4ad599a571245acf51c7b14b87907b
Brett Buskirk <buskirkbrett8@gmail.com>
2024-09-12 00:45:54 -0400

    Aesthetic changes

--- social_frontend/src/App.js
+++ social_frontend/src/App.js
@@ -4,12 +4,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import CreatePost from './CreatePost';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
     return (
         <Router>
             <div className="app">
+	        <div style={{ display: 'flex', alignItems: 'center' }}> 
+                    <img src={logo} alt="MERN Social Logo" style={{ height: '75px', margin: '10px', borderRadius: '50%' }} /> 
+                    <h1 style={{ margin: 0 }}>MERN Social</h1>
+                </div>
                 <nav>
                     <ul>
                         <li>
